0fb093878e33937a235dc6f941e1a45a20998a1f
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-10-15 06:56:28 +0000

    render option el

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+import Option from "./components/Option";
+
+// https://dog.ceo/api/breed/affenpinscher/images/random
+// https://dog.ceo/api/breeds/list/all
+
+const BASE_URL = `https://dog.ceo/api/`;
+
+const breedListEl = document.querySelector("#data-breed-list");
+const imageEl = document.querySelector("img");
+
+// === MARK: Fetch
+function getDogsList() {
+  return fetch(`${BASE_URL}breeds/list/all`)
+    .then((res) => res.json())
+    .then((data) => data.message)
+    .catch((err) => console.error("error aagyi", err));
+}
+
+// TODO: Implement this
+function getDogImage(breed) {}
+
+// === MARK: Render
+function renderSelect() {
+  getDogsList().then((breedList) => {
+    for (let breed in breedList) {
+      breedListEl.appendChild(Option(breed));
+    }
+  });
+}
+
+renderSelect();
+
+function renderImage() {}
