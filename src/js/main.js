0cc55045db5bd32e865c8f3fee4ec896abc4b2f9
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-10-16 07:02:46 +0000

    load data only on inital render using localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,20 @@ const imageEl = document.querySelector("img");
 
 // === MARK: Fetch
 async function getDogsList() {
-  try {
-    const res = await fetch(`${BASE_URL}breeds/list/all`);
-    const data = await res.json();
-    return data.message;
-  } catch (err) {
-    console.error("Error occured", err);
+  let breeds = JSON.parse(localStorage.getItem("breeds"));
+
+  if (!breeds) {
+    try {
+      const res = await fetch(`${BASE_URL}breeds/list/all`);
+      const data = await res.json();
+      localStorage.setItem("breeds", JSON.stringify(data.message));
+      breeds = data.message;
+    } catch (err) {
+      console.error("Error occured", err);
+    }
   }
+
+  return breeds;
 }
 
 // Fetch a single dog breed image
@@ -37,6 +44,8 @@ async function renderSelect() {
   const dogsList = await getDogsList();
 
   const fragment = document.createDocumentFragment();
+
+  console.log(dogsList);
 
   Object.keys(dogsList).forEach((dogName) => {
     fragment.appendChild(Option(dogName));
